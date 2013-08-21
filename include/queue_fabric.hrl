92ff2bbb5a5e19c441e464732c405f943b453136
Anton Sizov <a.sysoff@gmail.com>
2013-08-21 11:46:07 +0300

    Add soap queues

--- include/queue_fabric.hrl
+++ include/queue_fabric.hrl
@@ -13,16 +13,24 @@
 -define(FUNNEL_CONTROL_Q, <<"pmm.funnel.server_control">>).
 -define(FUNNEL_NODE_Q_PREFIX, <<"pmm.funnel.nodes">>).
 
--define(K1API_AUTH_REQ_Q, <<"pmm.k1api.auth_request">>).
+-define(K1API_AUTH_REQ_Q, <<"pmm.http.auth_request">>).
 -define(K1API_AUTH_RESP_Q, <<"pmm.k1api.auth_response">>).
--define(K1API_DELIVERY_STATUS_REQ_Q, <<"pmm.k1api.delivery_status_request">>).
+-define(K1API_DELIVERY_STATUS_REQ_Q, <<"pmm.http.delivery_status_request">>).
 -define(K1API_DELIVERY_STATUS_RESP_Q, <<"pmm.k1api.delivery_status_response">>).
 -define(K1API_INCOMING_Q, <<"pmm.k1api.incoming">>).
 -define(K1API_RETRIEVE_SMS_REQ_Q, <<"pmm.k1api.retrieve_sms_request">>).
 -define(K1API_RETRIEVE_SMS_RESP_Q, <<"pmm.k1api.retrieve_sms_response">>).
--define(K1API_SMS_REQ_Q, <<"pmm.k1api.sms_request">>).
--define(K1API_SUBSCRIPTION_REQ_Q, <<"pmm.k1api.subscription_request">>).
+-define(K1API_SMS_REQ_Q, <<"pmm.http.sms_request">>).
+-define(K1API_SUBSCRIPTION_REQ_Q, <<"pmm.http.subscription_request">>).
 -define(K1API_SUBSCRIPTION_RESP_Q, <<"pmm.k1api.subscription_response">>).
--define(K1API_CONTROL_Q, <<"k1api.control">>).
+-define(K1API_CONTROL_Q, <<"pmm.k1api.control">>).
+
+-define(SOAP_CONTROL_Q, <<"pmm.soap.control">>).
+-define(SOAP_INCOMING_Q, <<"pmm.soap.incoming">>).
+-define(SOAP_DELIVERY_STATUS_RESP_Q, <<"pmm.soap.delivery_status_response">>).
+-define(SOAP_DELIVERY_STATUS_REQ_Q, ?K1API_DELIVERY_STATUS_REQ_Q).
+-define(SOAP_AUTH_RESP_Q, <<"pmm.soap.auth_response">>).
+-define(SOAP_AUTH_REQ_Q, ?K1API_AUTH_REQ_Q).
+-define(SOAP_SMS_REQ_Q, ?K1API_SMS_REQ_Q).
 
 -endif. % queue_fabric_hrl
