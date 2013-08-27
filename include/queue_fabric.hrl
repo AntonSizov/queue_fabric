b146d91c0531bde4e04894dc7d33b5bb9264c79d
Anton Sizov <a.sysoff@gmail.com>
2013-08-27 12:25:16 +0300

    Add queues specs

--- include/queue_fabric.hrl
+++ include/queue_fabric.hrl
@@ -1,17 +1,56 @@
 -ifndef(queue_fabric_hrl).
 -define(queue_fabric_hrl, defined).
 
+-include_lib("rabbit_common/include/rabbit_framing.hrl").
+
 -define(JUST_CONTROL_Q, <<"pmm.just.control">>).
 -define(JUST_GTW_Q_PREFIX, <<"pmm.just.gateway">>).
 -define(RECEIPT_SMS_Q, <<"pmm.just.receipt_sms">>).
+-define(RECEIPT_SMS_Q_SPEC,
+	#'queue.declare'{
+		queue = ?RECEIPT_SMS_Q,
+		durable = true
+	}).
 -define(RESPONSE_SMS_Q, <<"pmm.just.sms_response">>).
+-define(RESPONSE_SMS_Q_SPEC,
+	#'queue.declare'{
+		queue = ?RESPONSE_SMS_Q,
+		durable = true
+	}).
 -define(INCOMING_SMS_Q, <<"pmm.just.incoming_sms">>).
+-define(INCOMING_SMS_Q_SPEC,
+	#'queue.declare'{
+		queue = ?INCOMING_SMS_Q,
+		durable = true
+	}).
+
 
 -define(FUNNEL_AUTH_Q, <<"pmm.funnel.auth">>).
+-define(FUNNEL_AUTH_Q_SPEC,
+	#'queue.declare'{
+		queue = ?FUNNEL_AUTH_Q
+	}).
 -define(FUNNEL_BATCHES_Q, <<"pmm.funnel.sms_request">>).
+-define(FUNNEL_BATCHES_Q_SPEC,
+	#'queue.declare'{
+		queue = ?FUNNEL_BATCHES_Q,
+		durable = true
+	}).
 -define(FUNNEL_EVENTS_Q, <<"pmm.funnel.events">>).
+-define(FUNNEL_EVENTS_Q_SPEC,
+	#'queue.declare'{
+		queue = ?FUNNEL_EVENTS_Q,
+		durable = true
+	}).
 -define(FUNNEL_CONTROL_Q, <<"pmm.funnel.control">>).
+-define(FUNNEL_CONTROL_Q_SPEC,
+	#'queue.declare'{
+		queue = ?FUNNEL_CONTROL_Q,
+		exclusive = true,
+		auto_delete = true
+	}).
 -define(FUNNEL_NODE_Q_PREFIX, <<"pmm.funnel.nodes">>).
+
 
 -define(K1API_AUTH_REQ_Q, <<"pmm.http.auth_request">>).
 -define(K1API_AUTH_RESP_Q, <<"pmm.k1api.auth_response">>).
@@ -24,6 +63,7 @@
 -define(K1API_SUBSCRIPTION_REQ_Q, <<"pmm.http.subscription_request">>).
 -define(K1API_SUBSCRIPTION_RESP_Q, <<"pmm.k1api.subscription_response">>).
 -define(K1API_CONTROL_Q, <<"pmm.k1api.control">>).
+
 
 -define(SOAP_CONTROL_Q, <<"pmm.soap.control">>).
 -define(SOAP_INCOMING_Q, <<"pmm.soap.incoming">>).
